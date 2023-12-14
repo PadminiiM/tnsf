457cec9c3496939b2755dfe62023bc82d2322dcc
PadminiiM <137522241+PadminiiM@users.noreply.github.com>
2023-12-14 15:39:46 +0530

    Add files via upload

---
+++ For_each.java
@@ -0,0 +1,16 @@
+package tnsf;
+
+public class For_each {
+
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5};
+		for(int k:a) {
+			System.out.println(k);
+		}
+		String[] str = {"Hello","Hi"};
+		for(String p:str) {
+			System.out.print(p+" ");
+		}
+	}
+}
+
